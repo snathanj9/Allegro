b0113e173318d63b656d928154babcc5c59393bf
snathanj9 <snathanj9@gmail.com>
2016-03-14 19:23:00 -0400

    Added Allegro functionality

--- Project2/Project2/Main.cpp
+++ Project2/Project2/Main.cpp
@@ -1,9 +1,22 @@
 #include "will.h"
-
+#include "allegro5\allegro.h"
 
 int main()
 {
-	std::cout << ":D" << std::endl;
-	willFunc();
-	system("PAUSE");
+	al_init();
+
+	ALLEGRO_DISPLAY *display = nullptr;
+	al_set_app_name("Hello World from Allegro 5.1!");
+	display = al_create_display(640, 480);
+	if (display == nullptr)
+	{
+		std::cerr << "Well, something is not working..." << std::endl;
+		al_rest(5.0);
+		return EXIT_FAILURE;
+	}
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	al_flip_display();
+	al_rest(5.0);
+	return 0;
 }
